97654c32d02dad78ebaeaae12e82ae0419416f31
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2023-05-30 12:22:25 +0200

    Last commit before code review - Removed load model functionality - Removed PHP-stuff - Changes to form interaction when adding new data - Added comments😊
    Known bugs:
    - When adding options to existing products sometimes it adds double

--- includes/js/scratch.js
+++ includes/js/scratch.js
@@ -1,27 +1,26 @@
 // Grab all the DOM elements
-const video = document.getElementById('video');
-const videoStatus = document.getElementById('video-status');
-const loading = document.getElementById('loading');
+const video = document.getElementById('video'); // Video element
+const videoStatus = document.getElementById('video-status'); // Element to display video status
+const loading = document.getElementById('loading'); // Element to display loading status
 
-const addInfoButton = document.getElementById('add-info-button');
-const addImageButton = document.getElementById('add-image-button');
-const imageFilesInput = document.getElementById('training-data');
+const addInfoButton = document.getElementById('add-info-button'); // Button to add new information
+const addImageButton = document.getElementById('add-image-button'); // Button to add new image
+const imageFilesInput = document.getElementById('training-data'); // Input for image files
 
-const source = document.getElementById('source');
-let trainingSource = "image";
+const source = document.getElementById('source'); // Source selection element
+let trainingSource = 'image'; // Variable to store the selected training source
+let newOrExisting = 'new'; // Variable to store whether new or existing data is selected
 
-const train = document.getElementById('train');
-const saveButton = document.getElementById('save-button');
-const loss = document.getElementById('loss');
-const result = document.getElementById('result');
-const confidence = document.getElementById('confidence');
-const predict = document.getElementById('predict');
+const train = document.getElementById('train'); // Train button
+const saveButton = document.getElementById('save-button'); // Save button
+const loss = document.getElementById('loss'); // Element to display loss
+const result = document.getElementById('result'); // Element to display prediction result
+const confidence = document.getElementById('confidence'); // Element to display prediction confidence
+const predict = document.getElementById('predict'); // Predict button
 
-const productSelector = document.getElementById('product');
-const sizeTypeSelector = document.getElementById('size-type');
-const sizeInput = document.getElementById('size');
-
-const modelURL = '../training/model/model.json'
+const productSelector = document.getElementById('product'); // Product selection element
+const sizeTypeSelector = document.getElementById('size-type'); // Size type selection element
+const sizeInput = document.getElementById('size'); // Size input element
 
 //
 // ML5 related code
@@ -32,7 +31,7 @@ let totalLoss = 0;
 
 // Create a webcam capture
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
         video.srcObject = stream;
         video.onloadedmetadata = () => {
             video.play();
@@ -43,11 +42,6 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 // A function to be called when the model has been loaded
 function modelLoaded() {
     loading.innerText = 'Model loaded!';
-    classifier.load(modelURL, function () {
-        loading.innerText = 'Model and custom model loaded!';
-        // add this to client-side eventually
-        // classifier.classify(gotResults);
-    })
 }
 
 // Extract the already learned features from MobileNet
@@ -61,7 +55,6 @@ function videoReady() {
 }
 
 // When the train button is pressed, train the classifier
-// With all the given headphone, phone and bottle images
 train.onclick = function () {
     classifier.train(function (lossValue) {
         if (lossValue) {
@@ -94,30 +87,38 @@ function gotResults(err, results) {
 // Start predicting when the predict button is clicked
 predict.onclick = function () {
     classifier.classify(gotResults);
-}
+};
 
-addInfoButton.addEventListener('click', addInfo)
-addImageButton.addEventListener('click', addImage)
+// Event listeners for adding new data
+addInfoButton.addEventListener('click', addNewData);
+addImageButton.addEventListener('click', addNewData);
 
-function addInfo(e) {
-    e.preventDefault()
-    let productInformation = JSON.stringify({
-        'brand': document.getElementById('brand').value,
-        'name': document.getElementById('name').value,
-        'description': document.getElementById('description').value,
-        'image': document.getElementById('image').value,
-        'averageShelfLife': document.getElementById('average-shelf-life').value,
-        'averageShelfLifeType': document.getElementById('average-shelf-life-type').value,
-        'size': document.getElementById('size').value,
-        'sizeType': document.getElementById('size-type').value,
-        'category': document.getElementById('product-category').value,
-        'status': 0,
-        'userId': 0,
-    })
+function addNewData(e) {
+    e.preventDefault();
+    let productInformation;
 
-    console.log(productInformation)
+    if (document.getElementById('product').value === 'new') {
+        // Create a new product information object
+        productInformation = JSON.stringify({
+            'brand': document.getElementById('brand').value,
+            'name': document.getElementById('name').value,
+            'description': document.getElementById('description').value,
+            'image': document.getElementById('image').value,
+            'averageShelfLife': document.getElementById('average-shelf-life').value,
+            'averageShelfLifeType': document.getElementById('average-shelf-life-type').value,
+            'size': document.getElementById('size').value,
+            'sizeType': document.getElementById('size-type').value,
+            'category': document.getElementById('product-category').value,
+            'status': 0,
+            'userId': 0,
+        });
+    } else {
+        // Use existing product information
+        productInformation = document.getElementById('item-select').value;
+    }
 
-    if (trainingSource !== "image") {
+    if (trainingSource !== 'image') {
+        // Add image from video to the classifier
         classifier.addImage(video, productInformation);
     } else {
         const files = imageFilesInput.files;
@@ -126,39 +127,24 @@ function addInfo(e) {
                 const img = new Image();
                 img.src = URL.createObjectURL(file);
 
-                img.style.height = '200px'
-                img.style.width = '200px'
+                img.style.height = '200px';
+                img.style.width = '200px';
                 img.addEventListener('load', () => {
                     classifier.addImage(img, productInformation);
                 });
             }
-        } else {
-            alert('Please select at least one image file.');
-        }
-    }
-}
 
-function addImage(e) {
-    e.preventDefault()
+            if (newOrExisting === 'new') {
+                // Add a new option to the product selection dropdown
+                let option = document.createElement('option');
+                let product = JSON.parse(productInformation);
+                option.innerHTML = `${product.brand} ${product.name} ${product.size} ${product.sizeType}`;
+                option.value = productInformation;
+                document.getElementById('item-select').appendChild(option);
 
-    let productInformation = document.getElementById('item-select').value
-
-    console.log(productInformation)
-
-    if (trainingSource !== "image") {
-        classifier.addImage(video, productInformation);
-    } else {
-        const files = imageFilesInput.files;
-        if (files.length > 0) {
-            for (let file of files) {
-                const img = new Image();
-                img.src = URL.createObjectURL(file);
-
-                img.style.height = '200px'
-                img.style.width = '200px'
-                img.addEventListener('load', () => {
-                    classifier.addImage(img, productInformation);
-                });
+                productSelector.value = 'existing';
+                document.getElementById('hidden-if-new').style.display = 'block';
+                document.getElementById('hidden-if-existing').style.display = 'none';
             }
         } else {
             alert('Please select at least one image file.');
@@ -167,73 +153,63 @@ function addImage(e) {
 }
 
 //
-//Form Related code
+// Form Related code
 //
 
-//Do stuff when page is loaded
-document.addEventListener("DOMContentLoaded", function () {
+// Do stuff when the page is loaded
+document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('hidden-if-new').style.display = 'none';
-})
+});
 
+// Event listener for product selection change
 productSelector.addEventListener('change', (e) => {
     if (e.target.value === 'new') {
+        newOrExisting = 'new';
         document.getElementById('hidden-if-new').style.display = 'none';
         document.getElementById('hidden-if-existing').style.display = 'block';
     } else {
+        newOrExisting = 'existing';
         document.getElementById('hidden-if-new').style.display = 'block';
         document.getElementById('hidden-if-existing').style.display = 'none';
     }
-})
+});
 
+// Event listener for source selection change
 source.addEventListener('change', (e) => {
     trainingSource = e.target.value;
-    if (e.target.value !== "image") {
+    if (e.target.value !== 'image') {
         document.getElementById('training-data').style.display = 'none';
         document.getElementById('training-data-label').style.display = 'none';
     } else {
         document.getElementById('training-data').style.display = 'block';
         document.getElementById('training-data-label').style.display = 'block';
     }
-})
+});
 
+// Event listener for size type selection change
 sizeTypeSelector.addEventListener('change', (e) => {
-    document.getElementById('size-notation').innerHTML = e.target.value
-    UpdateReadableSize()
-})
+    document.getElementById('size-notation').innerHTML = e.target.value;
+    UpdateReadableSize();
+});
 
-document.getElementById('size').addEventListener('change', UpdateReadableSize)
+// Event listener for size input change
+document.getElementById('size').addEventListener('change', UpdateReadableSize);
 
+// Update the readable size based on the selected size type
 function UpdateReadableSize() {
     if (sizeTypeSelector.value === 'ML') {
-        document.getElementById('readable-size').innerHTML = ` (= ${sizeInput.value / 1000}L)`
+        document.getElementById('readable-size').innerHTML = ` (= ${sizeInput.value / 1000}L)`;
     } else if (sizeTypeSelector.value === 'Gram') {
-        document.getElementById('readable-size').innerHTML = ` (= ${sizeInput.value / 1000}KG)`
+        document.getElementById('readable-size').innerHTML = ` (= ${sizeInput.value / 1000}KG)`;
     }
 }
 
+// Event listener for image input change
 document.getElementById('image').addEventListener('change', (e) => {
-    document.getElementById('preview-image').src = e.target.value
-})
+    document.getElementById('preview-image').src = e.target.value;
+});
 
+// Event listener for average shelf life type selection change
 document.getElementById('average-shelf-life-type').addEventListener('change', (e) => {
-    document.getElementById('date-notation').innerHTML = `dagen tot ${e.target.value}-datum`
-})
-
-//Add available data
-let availableLabels = [];
-
-fetch(modelURL)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        availableLabels = data.ml5Specs.mapStringToIndex;
-        for (let item of availableLabels) {
-            let option = document.createElement('option')
-            console.log(item)
-            let product = JSON.parse(item);
-            option.innerHTML = `${product.brand} ${product.name} ${product.size} ${product.sizeType}`;
-            option.value = item;
-            document.getElementById('item-select').appendChild(option)
-        }
-    });
+    document.getElementById('date-notation').innerHTML = `dagen tot ${e.target.value}-datum`;
+});
