2f7faea7943850b45a60a78823c25c4a4883114e
Diego <d.lens2712@gmail.com>
2023-06-13 00:53:43 +0200

    put listitem in component

--- components/listpage.js
+++ components/listpage.js
@@ -2,9 +2,9 @@ import { FlatList, Image, Pressable, SafeAreaView, StyleSheet, Text, View } from
 import React, { useState } from "react";
 import ListpageButton from "./listpage-button";
 import DropdownFilter from "./dropdown-filter";
+import ListviewItem from "./listview-item";
 
 export default function Listpage() {
-  const [selectedId, setselectedId] = useState([]);
   return (
     <React.Fragment>
       <DropdownFilter />
@@ -17,29 +17,7 @@ export default function Listpage() {
         />
       </View>
       <SafeAreaView style={styles.container}>
-        <FlatList data={[
-          {id: '1', name: 'milk', tht: '01-02-1238', amount: '1'},
-          {id: '2', name: 'butter', tht: '24-06-1984', amount: '3'},
-          {id: '3', name: 'something else', tht: '27-12-1998', amount: '5'}]}
-          extraData={{}}
-          renderItem={({item}) => (
-            <Pressable style={!selectedId.includes(item.id) ? styles.listItemWrapper : [styles.listItemWrapper, {backgroundColor: 'red'}]} onLongPress={() => {
-              if (!selectedId.includes(item.id)) {
-                setselectedId([...selectedId, item.id]);
-                return;
-              } else {
-                setselectedId(selectedId.filter((id) => id !== item.id));
-                return;
-              }
-            }}>
-                <Image source={require('../assets/icon.png')} style={{width: 50, height: 50}}/>
-                <View style={styles.listNameWrapper}>
-                  <Text>{item.name}</Text>
-                  <Text>THT: <Text style={styles.alert}>{item.tht}</Text></Text>
-                </View>
-                <Text style={styles.amount}>{item.amount}X</Text>
-            </Pressable>
-          )}/>
+        <ListviewItem />
       </SafeAreaView>
     </React.Fragment>
   );
---
+++ components/listview-item.js
@@ -0,0 +1,54 @@
+import { FlatList, Image, Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+
+export default function ListviewItem() {
+    const [selectedId, setselectedId] = useState([]);
+    return(
+        <FlatList data={[
+            {id: '1', name: 'milk', tht: '01-02-1238', amount: '1'},
+            {id: '2', name: 'butter', tht: '24-06-1984', amount: '3'},
+            {id: '3', name: 'something else', tht: '27-12-1998', amount: '5'}]}
+            extraData={{}}
+            renderItem={({item}) => (
+              <Pressable style={!selectedId.includes(item.id) ? styles.listItemWrapper : [styles.listItemWrapper, {backgroundColor: 'red'}]} onLongPress={() => {
+                if (!selectedId.includes(item.id)) {
+                  setselectedId([...selectedId, item.id]);
+                  return;
+                } else {
+                  setselectedId(selectedId.filter((id) => id !== item.id));
+                  return;
+                }
+              }}>
+                  <Image source={require('../assets/icon.png')} style={{width: 50, height: 50}}/>
+                  <View style={styles.listNameWrapper}>
+                    <Text>{item.name}</Text>
+                    <Text>THT: <Text style={styles.alert}>{item.tht}</Text></Text>
+                  </View>
+                  <Text style={styles.amount}>{item.amount}X</Text>
+              </Pressable>
+            )}/>
+    )
+}
+
+const styles = StyleSheet.create({
+    listItemWrapper: {
+      padding: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: "#aaa",
+      flexDirection: "row",
+      gap: 10,
+    },
+    listNameWrapper: {
+      alignSelf: "center",
+    },
+    amount: {
+      fontSize: 26,
+      alignSelf: "center",
+      marginLeft: 'auto',
+    },
+    alert: {
+      color: "red",
+      fontWeight: "bold",
+    },
+  });
+  
