9aa9cc8a19a811a0275b2384bdc50d4086a9222c
DiegoLens1 <d.lens2712@gmail.com>
2023-06-15 15:49:16 +0200

    added fetch for db content

--- components/listview-item.js
+++ components/listview-item.js
@@ -1,52 +1,28 @@
 import { FlatList, Image, Pressable, StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function ListviewItem() {
     const [selectedId, setselectedId] = useState([]);
-    function UpdateDatabase(label) {
-      const url = '../includes/back-end-handlers/recognition-handler.php';
-      fetch(url, {
-          method: 'GET',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ label: label }),
-      })
-          .then(response => {
-              if (response.ok) {
-                  return response.json();
-              } else {
-                  errorSound.play();
-                  throw new Error('Error updating label');
-              }
-          })
-          .then(responseText => {
-              successSound.play();
-              console.log(responseText);
-          })
-          .catch(error => {
-              errorSound.play();
-              console.error(error);
-          });
-  }
-  const [isloaded, setisLoaded] = useState(false);
+    const [data, setData] = useState([]);
+    const [isloaded, setisLoaded] = useState(false);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+      fetchData();
+    }, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        'https://stud.hosted.hr.nl/1000200/crimeWebservice/'
-      );
-      const jsonData = await response.json();
-      setcrimeData(jsonData);
-      setisLoaded(true)
-    } catch (error) {
-      console.error(error);
+    async function fetchData() {
+      const url = 'http://145.137.17.236/tle2team6-ml5-2022-2023/includes/back-end-handlers/list-item-get-handler.php';
+      try {
+        const response = await fetch(url)
+        const jsonData = await response.json();
+        console.log(jsonData)
+        setData(jsonData);
+        setisLoaded(true)
+      } catch(error){
+        
+        console.error(error);
+      }
     }
-  };
 
   if(!isloaded){
     return(
@@ -56,17 +32,7 @@ export default function ListviewItem() {
     )
   }
     return(
-        <FlatList data={[
-            {id: '1', name: 'milk', tht: '01-02-1238', amount: '1'},
-            {id: '2', name: 'butter', tht: '24-06-1984', amount: '3'},
-            {id: '3', name: 'something else', tht: '27-12-1998', amount: '5'},
-            {id: '4', name: 'something else', tht: '27-12-1998', amount: '5'},
-            {id: '5', name: 'something else', tht: '27-12-1998', amount: '5'},
-            {id: '6', name: 'something else', tht: '27-12-1998', amount: '5'},
-            {id: '7', name: 'something else', tht: '27-12-1998', amount: '5'},
-            {id: '8', name: 'something else', tht: '27-12-1998', amount: '5'},
-            {id: '9', name: 'something else', tht: '27-12-1998', amount: '5'},
-            {id: '10', name: 'something else', tht: '27-12-1998', amount: '5'},]}
+        <FlatList data={data}
             extraData={{}}
             renderItem={({item}) => (
               <Pressable style={!selectedId.includes(item.id) ? styles.listItemWrapper : [styles.listItemWrapper, {backgroundColor: 'lightgrey'}]} onLongPress={() => {
@@ -81,9 +47,9 @@ export default function ListviewItem() {
                   <Image source={require('../assets/icon.png')} style={{width: 50, height: 50}}/>
                   <View style={styles.listNameWrapper}>
                     <Text>{item.name}</Text>
-                    <Text>THT: <Text style={styles.alert}>{item.tht}</Text></Text>
+                    <Text>THT: <Text style={styles.alert}>{item.average_shelf_life}</Text></Text>
                   </View>
-                  <Text style={styles.amount}>{item.amount}X</Text>
+                  <Text style={styles.amount}>3X</Text>
               </Pressable>
             )}/>
     )
