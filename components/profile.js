0c105eb6e6c92a2a1a538fcc4762b3631a23e4f9
Danz <DvRossen@outlook.com>
2023-06-17 21:13:20 +0200

    layout profile fixed, buttons only log "test"

--- components/profile.js
+++ components/profile.js
@@ -1,17 +1,88 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { TouchableHighlight, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
+
+const profileName = "testUser";
+const email = "test@email.com";
+const accountDate = Date("1-1-23");
 
 export default function Profile() {
   return (
-    <View style={styles.container}>
-      <Text>Your Profile will load here</Text>
+    <View style={styles.bg}>
+      <View style={[styles.infoCard, styles.card]}>
+        <Text style={styles.labelText}>Username:</Text>
+        <Text>{profileName}</Text>
+        <Text style={styles.labelText}>Email:</Text>
+        <Text>{email}</Text>
+        <Text style={styles.labelText}>Creation date:</Text>
+        <Text>{accountDate}</Text>
+      </View>
+
+      <View style={[styles.card, styles.buttonCard]}>
+        <TouchableHighlight
+          underlayColor="#eaeaea"
+          style={[styles.button, styles.editButton]}
+          onPress={() => console.log("test")}
+        >
+          <Text style={styles.editText}>edit</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          underlayColor="#eaeaea"
+          style={[styles.button, styles.deleteButton]}
+          onPress={() => console.log("test")}
+        >
+          <Text style={styles.deleteText}>delete</Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  bg: {
     backgroundColor: "#eaeaea",
     height: "100%",
+    width: "100%",
+    alignItems: "center",
+  },
+  labelText: {
+    fontWeight: "bold",
+  },
+  card: {
+    backgroundColor: "#fff",
+    width: "90%",
+    borderRadius: "10",
+    padding: 20,
+  },
+  infoCard: {
+    marginTop: 25,
+    marginBottom: 25,
+  },
+  buttonCard: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  button: {
+    backgroundColor: "#fff",
+    borderRadius: "10",
+    width: 100,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  deleteButton: {
+    borderWidth: 1,
+    borderColor: "red",
+  },
+  deleteText: {
+    color: "red",
+    fontWeight: "bold",
+  },
+  editButton: {
+    borderWidth: 1,
+    borderColor: "#629ade",
+  },
+  editText: {
+    color: "#629ade",
+    fontWeight: "bold",
   },
 });
