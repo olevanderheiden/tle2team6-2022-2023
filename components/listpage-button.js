d387b5fdb36aaeb59361dd27a4be33d99ab4dfbd
Danz <DvRossen@outlook.com>
2023-06-25 00:17:49 +0200

    added button handler for edit and delete in list-page button

--- components/listpage-button.js
+++ components/listpage-button.js
@@ -4,11 +4,48 @@ import React from "react";
 export default function ListpageButton({ name }) {
   return (
     <React.Fragment>
-      <Pressable onPress={() => {}}>
+      <Pressable
+        onPress={() => {
+          if ((name = "delete")) {
+            buttonHandler(
+              "https://stud.hosted.hr.nl/1000200/fridge_friend/back-end-handlers/delete-product-user-handler.php"
+            );
+          } else if ((name = "edit")) {
+            buttonHandler(
+              "https://stud.hosted.hr.nl/1000200/fridge_friend/back-end-handlers/product-user-update.php"
+            );
+          }
+        }}
+      >
         <Text style={styles.buttonStyle}>{name}</Text>
       </Pressable>
     </React.Fragment>
   );
+  async function buttonHandler(url) {
+    if ((name = "delete")) {
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    } else if ((name = "edit")) {
+      try {
+        const response = await fetch(url, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
 }
 
 const styles = StyleSheet.create({
