cce3ee00f09d126d495908f67b2faa765d3d2601
Danz <DvRossen@outlook.com>
2023-06-25 16:42:37 +0200

    edit and delete now touchable highlights because :sparkles: interactions

--- components/listpage-button.js
+++ components/listpage-button.js
@@ -1,22 +1,30 @@
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, StyleSheet, Text, TouchableHighlight } from "react-native";
 import React from "react";
 
 export default function ListpageButton({ name, buttonHandler }) {
   return (
     <React.Fragment>
-      <Pressable onPress={buttonHandler}>
-        <Text style={styles.buttonStyle}>{name}</Text>
-      </Pressable>
+      <TouchableHighlight
+        style={styles.button}
+        underlayColor="#fff"
+        onPress={buttonHandler}
+      >
+        <Text style={styles.buttonText}>{name}</Text>
+      </TouchableHighlight>
     </React.Fragment>
   );
 }
 
 const styles = StyleSheet.create({
-  buttonStyle: {
-    padding: 8,
+  button: {
+    alignItems: "center",
+    width: 100,
     borderWidth: 1,
     borderColor: "#aaa",
     borderRadius: 20,
+  },
+  buttonText: {
+    padding: 8,
     fontSize: 18,
     color: "#629ade",
   },
